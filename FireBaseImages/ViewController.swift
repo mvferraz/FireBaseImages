c563192a8515b5f41b3b1bee22fd45af876114ea
Marcus Ferraz <marcus.ferraz@marcuss-imac.local>
2018-03-06 12:12:24 -0800

    writing firebase code

--- FireBaseImages/ViewController.swift
+++ FireBaseImages/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var uploadImage: UIImageView!
+    
+    @IBOutlet weak var downloadImage: UIImageView!
+    let fileName = "boat.jpg"
+    var imageReference: StorageReference{
+        return Storage.storage().reference().child("images")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    @IBAction func uploadButtonPressed(_ sender: UIButton) {
+        guard let image = uploadImage.image else{
+            print("Image not uploaded")
+            return
+        }
+        
+        guard let imageData = UIImageJPEGRepresentation(image, 1) else {
+            print("Error ImageData")
+            return
+        }
+        
+        let uploadImageReference = imageReference.child(fileName)
+        let uploadTask = uploadImageReference.putData(imageData, metadata: nil) { (metadata, error) in
+            print(metadata ?? "No Metadata")
+            print(error ?? "No Error")
+        }
+        uploadTask.observe(.progress) { (snapshot) in
+            print(snapshot.progress ?? "No More Progress")
+        }
+        
+        uploadTask.resume()
+    }
+    
+    @IBAction func downloadButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
